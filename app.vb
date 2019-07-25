67c3d0e0c7ac72968a2d7d945b602e8c10d4f17b
Hanrie <development2@loadtech.co.za>
2019-07-25 16:28:40 +0200

    B3variable

--- app.vb
+++ app.vb
@@ -3,4 +3,6 @@
 dim variable1 as string = "This is my string"
 dim variable2 as string = "This is my second variable string"
 
+dim b3 as string "This is now in branch 3"
+
 ''seems like it comes with its own code editor
