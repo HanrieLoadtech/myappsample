c47c598453253d8962c4c36d020d1e2bc8456a48
Hanrie <development2@loadtech.co.za>
2019-07-25 16:23:35 +0200

    Added second variable
    added it to app.vb

--- app.vb
+++ app.vb
@@ -1,5 +1,6 @@
 ''changing the master app in the master branch
 
 dim variable1 as string = "This is my string"
+dim variable2 as string = "This is my second variable string"
 
 ''seems like it comes with its own code editor
